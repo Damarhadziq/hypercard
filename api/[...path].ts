67fd8519b3320c7128ebd29b48d7dd91e3e0b015
damarhadziq <damarhadziq1234@gmail.com>
2026-06-14 21:45:57 +0700

    rev vercel

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,8 @@
 import app from '../apps/api/src/app';
 
-export default app;
+export default function handler(
+  req: Parameters<typeof app>[0],
+  res: Parameters<typeof app>[1]
+) {
+  return app(req, res);
+}
